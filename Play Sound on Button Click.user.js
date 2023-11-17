250b7ea60af022d081dd7436b36d4373a7c9cd75
LemonIsAHe1 <115420638+LemonIsAHe1@users.noreply.github.com>
2023-11-17 18:48:58 -0500

    Update Play Sound on Button Click.user.js
    Finding buttons, detecting off timer not working.

--- Play Sound on Button Click.user.js
+++ Play Sound on Button Click.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Play Sound on Button Click
 // @namespace    http://your.namespace.here
-// @version      0.1
+// @version      0.2
 // @description  Play a sound when a button is clickable
 // @author       LemonIsAHe
 // @match        https://fair.kaliburg.de/*
@@ -12,7 +12,7 @@
     'use strict';
 
     // Define the sound file URL
-    const soundURL = 'https://www.soundurl.com/your-sound-file.mp3'; // Replace with your sound file URL
+    const soundURL = 'https://cdn.discordapp.com/attachments/932894220509909033/1175215629758898296/337049__shinephoenixstormcrow__320655__rhodesmas__level-up-01.mp3';
 
     // Create an audio element
     const audioElement = new Audio(soundURL);
@@ -22,14 +22,29 @@
         audioElement.play();
     }
 
-    // Add a click event listener to the button
-    function addButtonClickListener() {
-    const button = document.querySelector('.rounded-md.border-solid.border-1.h-8.px-2.bg-button-bg.text-button-text.border-button-border.disabled:select-none.disabled:opacity-50.disabled:grayscale.disabled:pointer-events-none.hover:bg-button-bg-hover.hover:text-button-text-hover.w-full.rounded-r-none.whitespace-nowrap[data-tutorial="multi"]');
-    if (button) {
-        button.addEventListener('click', playSound);
+    // Function to add a click event listener to a button with a specific selector
+    function addButtonClickListener(selector) {
+        const button = document.querySelector(selector);
+        if (button) {
+            button.addEventListener('click', playSound);
+        }
     }
-}
 
-    // Call the function to add the click event listener when the DOM is ready
-    document.addEventListener('DOMContentLoaded', addButtonClickListener);
+    // Function to handle mutations and check for the presence of buttons
+    function handleMutations(mutationsList) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                addButtonClickListener('button[class*="rounded-md"][data-tutorial="bias"]');
+                addButtonClickListener('button[class*="rounded-md"][data-tutorial="multi"]');
+            }
+        }
+    }
+
+    // Set up a MutationObserver to observe changes in the DOM
+    const observer = new MutationObserver(handleMutations);
+
+    // Start observing the target node for changes in child elements
+    const targetNode = document.body;
+    const observerConfig = { childList: true, subtree: true };
+    observer.observe(targetNode, observerConfig);
 })();
