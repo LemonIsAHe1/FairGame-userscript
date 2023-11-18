383bd6abcb93f464b4ad7def9bc95ca834ad6731
LemonIsAHe1 <115420638+LemonIsAHe1@users.noreply.github.com>
2023-11-17 19:07:50 -0500

    Ver 0.6 with debug gui togglable with ctrl+[

--- Play Sound on Button Click.user.js
+++ Play Sound on Button Click.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Play Sound on Button Click
 // @namespace    http://your.namespace.here
-// @version      0.2
+// @version      0.6
 // @description  Play a sound when a button is clickable
 // @author       LemonIsAHe
 // @match        https://fair.kaliburg.de/*
@@ -22,20 +22,18 @@
         audioElement.play();
     }
 
-    // Function to add a click event listener to a button with a specific selector
-    function addButtonClickListener(selector) {
-        const button = document.querySelector(selector);
-        if (button) {
-            button.addEventListener('click', playSound);
-        }
-    }
-
-    // Function to handle mutations and check for the presence of buttons
+    // Function to handle mutations and check for changes in button attributes
     function handleMutations(mutationsList) {
         for (const mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                addButtonClickListener('button[class*="rounded-md"][data-tutorial="bias"]');
-                addButtonClickListener('button[class*="rounded-md"][data-tutorial="multi"]');
+            if (mutation.type === 'attributes' && mutation.attributeName === 'disabled') {
+                const targetButton = mutation.target;
+                if (!targetButton.disabled) {
+                    // Button is no longer disabled, add the click event listener
+                    targetButton.addEventListener('click', playSound);
+                } else {
+                    // Button is disabled, remove the click event listener if it was added previously
+                    targetButton.removeEventListener('click', playSound);
+                }
             }
         }
     }
@@ -43,8 +41,99 @@
     // Set up a MutationObserver to observe changes in the DOM
     const observer = new MutationObserver(handleMutations);
 
-    // Start observing the target node for changes in child elements
+    // Start observing the target node for changes in attributes
     const targetNode = document.body;
-    const observerConfig = { childList: true, subtree: true };
+    const observerConfig = { attributes: true, attributeFilter: ['disabled'], subtree: true };
     observer.observe(targetNode, observerConfig);
+
+    // Create GUI elements
+    const guiContainer = document.createElement('div');
+    guiContainer.style.position = 'fixed';
+    guiContainer.style.top = '20px';
+    guiContainer.style.left = '20px';
+    guiContainer.style.background = 'rgba(255, 255, 255, 0.8)';
+    guiContainer.style.padding = '10px';
+    guiContainer.style.borderRadius = '8px';
+    guiContainer.style.cursor = 'move';
+    guiContainer.draggable = true;
+    guiContainer.style.display = 'none'; // Initially hide the GUI
+
+    const guiTitle = document.createElement('div');
+    guiTitle.innerHTML = '<strong>Sound Player</strong>';
+    guiTitle.style.marginBottom = '8px';
+
+    const closeButton = document.createElement('span');
+    closeButton.innerHTML = '&times;';
+    closeButton.style.float = 'right';
+    closeButton.style.cursor = 'pointer';
+    closeButton.addEventListener('click', () => {
+        guiContainer.style.display = 'none'; // Hide the GUI on close
+    });
+
+    guiTitle.appendChild(closeButton);
+    guiContainer.appendChild(guiTitle);
+
+    const statusContainer = document.createElement('div');
+    statusContainer.style.marginBottom = '8px';
+
+    const statusText = document.createElement('div');
+    statusText.style.fontWeight = 'bold';
+
+    statusContainer.appendChild(statusText);
+    guiContainer.appendChild(statusContainer);
+
+    document.body.appendChild(guiContainer);
+
+    // Make the GUI movable
+    let isDragging = false;
+    let offsetX, offsetY;
+
+    guiContainer.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        offsetX = e.clientX - guiContainer.getBoundingClientRect().left;
+        offsetY = e.clientY - guiContainer.getBoundingClientRect().top;
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        if (isDragging) {
+            const x = e.clientX - offsetX;
+            const y = e.clientY - offsetY;
+            guiContainer.style.left = x + 'px';
+            guiContainer.style.top = y + 'px';
+        }
+    });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+
+    // Update the status information and play sound if a button is enabled
+    function updateStatus() {
+        const biasButton = document.querySelector('button[class*="rounded-md"][data-tutorial="bias"]');
+        const multiButton = document.querySelector('button[class*="rounded-md"][data-tutorial="multi"]');
+
+        const biasStatus = biasButton ? (biasButton.disabled ? 'Disabled' : 'Enabled') : 'Not Found';
+        const multiStatus = multiButton ? (multiButton.disabled ? 'Disabled' : 'Enabled') : 'Not Found';
+
+        statusText.innerHTML = `Bias Button: ${biasStatus}<br>Multi Button: ${multiStatus}`;
+
+        // Play sound if Bias Button is enabled
+        if (biasButton && !biasButton.disabled) {
+            playSound();
+        }
+    }
+
+    // Initial status update
+    updateStatus();
+
+    // Schedule periodic status updates
+    setInterval(updateStatus, 5000); // Update every 5 seconds
+
+    // Toggle GUI visibility on "Ctrl+[" key combination
+    document.addEventListener('keydown', (e) => {
+        if (e.ctrlKey && e.key === '[') {
+            guiContainer.style.display = guiContainer.style.display === 'none' ?
+                'block' : 'none';
+        }
+    });
 })();
